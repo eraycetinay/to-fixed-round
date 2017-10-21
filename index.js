1df13a57384307f8289b167f70e8cdaa91c28087
Eray Çetinay <eraycetinay@gmail.com>
2017-10-21 23:01:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 'use strict';
 
-Number.prototype.toFixedRound = function(ln,rn) {
+Number.prototype.toFixedRound = function(ln = 0, rn = 0) {
     var n = 0,
         l = 0;
-
-    n = parseFloat(this); 
+    n = parseFloat(this);
     if (Math.floor(this.valueOf()) == this.valueOf()) {
         l = 0;
     } else {
         l = this.toString().split(".")[1].length || 0;
     }
-    if (l > ln || (rn && l<ln))  {
+    if (ln > 0 && (l > ln || (rn && l < ln))) {
         n = n + (1 / Math.pow(10, ln));
     }
-    if(rn) {
-    	return parseFloat(n.toFixed(ln));
+    if (rn && ln > 0) {
+        return parseFloat(n.toFixed(ln));
     } else {
-    	return n.toFixed(ln);
+        return n.toFixed(ln);
     }
-
 };
